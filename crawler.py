ba10ee916a052f4152220af3d9c57e5669f9c70f
Bethany Anne Waldmann <bwaldmann@csdl.tamu.edu>
2009-06-08 12:29:43 -0500

    successfully finds and prints user/location pairs

--- crawler.py
+++ crawler.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python
 
 import commands
-import location
+from jk_loc import loc
 #from BeautifulSoup import BeautifulSoup
 #import re
 from optparse import OptionParser
 
 def main():
     (options, args) = parser.parse_args()
-    
-    users = open(options.f,'w')
-    users.write(commands.getoutput('ls '+options.s))
-    users.close()
-    
+    statout = commands.getstatusoutput('ls '+options.s+' > '+options.f)
     users = open(options.f,'r')
-    line = users.readline()
-    while line:
+    for line in users:
         user = options.s+line[0:-1]
         print "user: ",user
         page = open(user,'r')
-#        soup = BeautifulSoup(page)
-#        if options.v:
-#            print soup.prettify()
-#        list = soup.findAll('a', attrs={'href' : re.compile("\/[\w\?=]+")})
-#        if options.v:
-#            print "finding location for user ", user[0:-1], ":"
-#        loc = commands.getoutput('./location.py -v -f '+options.s+user)
-#        print loc, "\n"
+        contents = page.read()
         page.close()
-        line = users.readline()
+        location = loc(contents)
+        print location, "\n"
     users.close()
 
 parser = OptionParser()
-parser.add_option("-v", "--verbose", action="store_true", dest="v", default=False, help="turn on verbose mode")
-parser.add_option("-s", "--source", dest="s", metavar="DIR", default="/project/wdim/crawlData/good_sample/", help="directory to find user pages in")
+parser.add_option(
+    "-v",
+    "--verbose",
+    action="store_true",
+    dest="v",
+    default=False,
+    help="turn on verbose mode" )
+parser.add_option("-s",
+    "--source",
+    dest="s",
+    metavar="DIR",
+    default="/project/wdim/crawlData/good_sample/",
+    help="directory to find user pages in" )
 parser.add_option("-f", "--file", dest="f", metavar="FILE", default="users.txt", help="file to write user page names to")
 
 if __name__ == "__main__":
--- location.py
+++ location.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
-import urllib
 import re
 from optparse import OptionParser
+
+def loc(contents):
+    a = re.compile("(?i)<span class=\"adr\">[\w, -\.:]+</span>")
+    loc = a.search(contents)
+    if loc:
+        location = contents[loc.start():loc.end()]
+        return location[location.find(">")+1:location.rfind("<")]
 
 def main():
     (options, args) = parser.parse_args()
@@ -16,16 +22,20 @@ def main():
         contents = file.read()
         file.close()
         print "contents of file: ",contents
-#        l = re.compile("(?i)l:\w+|<span class=\"adr\">[\w, -\.:]+</span>|iphone: \d{1,2}\.\d{6},-?\d{1,2}\.\d{6}")
-#        result = l.findall(contents)
-#        print result
-        a = re.compile("(?i)<span class=\"adr\">[\w, -\.:]+</span>")
-        loc = str(a.findall(contents))
-        print loc[loc.find(">")+1:loc.rfind("<")]
+        print loc(contents)
 
 parser = OptionParser()
-parser.add_option("-v", "--verbose", action="store_true", dest="v", default=False, help="turn on verbose mode")
-parser.add_option("-f", "--file", dest="f", metavar="FILE", help="name of input file to search")
+parser.add_option("-v",
+    "--verbose",
+    action="store_true",
+    dest="v",
+    default=False,
+    help="turn on verbose mode")
+parser.add_option("-f",
+    "--file",
+    dest="f",
+    metavar="FILE",
+    help="name of input file to search")
         
 if __name__ == "__main__":
     main()
