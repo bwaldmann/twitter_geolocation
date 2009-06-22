a5745e21b27150dda0fb3e5c4cf42cd365593e05
Jeremy Kelley <jeremy@33ad.org>
2009-06-22 16:06:16 -0500

    refactored a bit

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import sys
 from time import sleep
 from os import pipe, fdopen, fork, close, getpid, getppid, kill
 import signal
- 
+
+MAX_PROCS = 5
+
  
 #inspired by http://mail.python.org/pipermail/python-list/2002-May/144522.html
 def is_running(pid):
-    try:
-        return kill(pid,0)
-    except:
-        return False
+    try: return kill(pid,0)
+    except: return False
 
 
 def childProc(num):
@@ -20,34 +20,31 @@ def childProc(num):
     sleep(10)
     print >>cfile,"*done %d*" % num
     cfile.close()
+    sys.exit()
 
- 
+
+def watch_the_children(proc): 
+    while len(proc)>=MAX_PROCS: #10 child processes running
+        for c in proc:
+            if not is_running(c):
+                print "  child process done: %d" % c
+                proc.remove(c)
+        print "SLEEPING %d processes running - waiting..." % len(proc)
+        sleep(3) #wait 3 seconds
+
+
 def main():
     log = open("test.log",'w')
-    tmp = range(20)
-    print tmp
     proc = []                           #child processes (current)
-    for num in tmp: #for each number 0-19
+    for num in range(10): #for each number 0-19
         cpid = fork()
         if cpid: #parent process
-            print "process %d starting child process: %d" % (getpid(),cpid)
+            print "NEWCHILD:%d PARENT:%d numOfKid:%d" % (cpid,getpid(),num)
             proc.append(cpid)
-            while len(proc)>=10: #10 child processes running
-                print "checking child processes"
-                print "  %s" % proc
-                for c in proc:
-                    if not is_running(c):
-                        print "  child process done: %d" % c
-                        proc.remove(c)
-                    else:
-                        print "  running %d" % c
-                print "%d processes running - waiting..." % len(proc)
-                sleep(3) #wait 3 seconds
+            watch_the_children(proc)
         else:
             childProc(num)
-            sys.exit()
-    sys.exit()
- 
- 
+
+
 if __name__ == "__main__":
     main()
